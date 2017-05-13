73c1c25fa1b3306979b59231f21700c49b2ade67
Robert F. Dickerson <rfdickerson@us.ibm.com>
2017-05-13 12:13:01 -0500

    Add some production mode env

--- server.js
+++ server.js
@@ -17,14 +17,13 @@ mongoose.connect('mongodb://localhost/comments');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(function (req, res, next) {
-  // res.setHeader('Access-Control-Allow-Origin', '*');
-  // res.setHeader('Access-Control-Allow-Credentials', 'true');
-  // res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
-  // res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
-  // res.setHeader('Cache-Control', 'no-cache');
-  next();
-});
+if (process.env.NODE_ENV == 'production') {
+  console.log('Using production mode');
+  var compression = require('compression');
+  app.use(compression());
+
+  app.use(express.static('build'));
+}
 
 router.get('/', function (req, res) {
   res.json({ message: 'API initialized' })
