67eb6a1d700d09c6aad20963daeac6520f34bf5c
root <root@ogre-js1.rtp.raleigh.ibm.com>
2018-06-08 13:09:34 -0400

    Automated Update

--- webpack.prod.js
+++ webpack.prod.js
@@ -17,13 +17,12 @@ const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
-  devtool: 'source-map',
-  plugins: [
-    new UglifyJSPlugin({
-      sourceMap: true
-    }),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': '"production"'
-    })
-  ]
+	mode: 'production',
+	optimization: {
+		minimizer: [
+			new UglifyJSPlugin({
+				sourceMap: true
+			}),
+		]	
+	}
 });
