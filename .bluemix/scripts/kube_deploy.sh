ac529d6a1ea858f9156272a5266da609a6cbf741
root <root@ogre-js1.rtp.raleigh.ibm.com>
2018-09-28 15:44:50 -0400

    Automated Update

--- .bluemix/scripts/kube_deploy.sh
+++ .bluemix/scripts/kube_deploy.sh
@@ -22,6 +22,10 @@ fi
 echo "Configuring Tiller (Helm's server component)"
 helm init --upgrade
 kubectl rollout status -w deployment/tiller-deploy --namespace=kube-system
+while [ "$(helm version | grep "Tiller")" != "" ]; do
+  echo "Waiting for server..."
+  sleep 10
+done
 helm version
 
 echo "CHART_NAME: $CHART_NAME"
